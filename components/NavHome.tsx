393f9390c445fa9e288e636c5afba4236769345e
wyw <charlotte.999666@gmail.com>
2024-11-30 22:49:24 +0200

    initiliaze

---
+++ components/NavHome.tsx
@@ -0,0 +1,17 @@
+import {Text,View} from 'react-native';
+
+const Index = () => {
+
+return(
+<View className={styles.container}>
+    <Text>Hello</Text>
+    </View>
+);
+};
+
+export default Index;
+
+
+const styles = {
+  container: `items-center flex-1 justify-center`,
+};
